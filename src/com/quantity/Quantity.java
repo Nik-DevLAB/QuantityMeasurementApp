3f2580c620e4a95968b6e4e0fdc262c64e97e547
Nik-DevLAB <na8647@srmist.edu.in>
2026-04-13 15:26:47 +0530

    UC1: Feet equality with test cases

---
+++ src/com/quantity/Quantity.java
@@ -0,0 +1,23 @@
+package com.quantity;
+
+import java.util.Objects;
+
+public class Quantity {
+    double value;
+    String unit;
+
+    public Quantity(double value, String unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Quantity q = (Quantity) obj;
+
+        return Double.compare(q.value, value) == 0 &&
+                Objects.equals(unit, q.unit);
+    }
+}
